769a597db937434e0a96cd95012c255e2a3d8f4a
AdrianCardonaCandil <adriancardonacandil@gmail.com>
2025-05-08 00:42:27 +0100

    marquees on menu section done

--- frontend/src/components/home/HomeGrid.jsx
+++ frontend/src/components/home/HomeGrid.jsx
@@ -4,28 +4,30 @@
  * @author Adrian Cardona Candil
  */
 
-import Front from "./Front"
+import Front from "./Front";
 import { useEffect, useState } from "react";
 import images from "../../assets/images/export";
 import Menu from "./Menu";
 
 // Mock information for the Front component
 const front = {
-    logo: "Perez & Beers",
-    message:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum viverra odio eget egestas.",
-    label: "Reserve your table",
-}
+  logo: "Perez & Beers",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum viverra odio eget egestas.",
+  label: "Reserve your table",
+};
 
 // Mock information for the Menu component
 const menu = {
-    sectionName: "Lorem ipsum",
-    information: {
-        title: "Lorem ipsum",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum viverra odio eget egestas.",
-    },
-    sectionButton: "Lorem ipsum dolor sit amet"
-}
+  sectionName: "Lorem ipsum",
+  information: {
+    title: "Lorem ipsum",
+    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a leo non nunc vestibulum
+        sodales vitae nec erat. Suspendisse potenti. Nunc quis ligula a massa lobortis ultricies. Mauris
+        imperdiet quam malesuada nibh euismod, quis bibendum magna laoreet`,
+  },
+  sectionButton: "Lorem ipsum dolor sit amet",
+};
 
 /**
  * @function HomeGrid
@@ -34,44 +36,54 @@ const menu = {
  */
 
 const HomeGrid = () => {
-    // Logic to handle the expanded state of the grid
-    const [expanded, setExpanded] = useState(false);
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            (entries) => {
-                entries.forEach((entry) => {
-                    if (entry.isIntersecting) {
-                        setExpanded(true);
-                    } else {
-                        setExpanded(false);
-                    }
-                });
-            },
-            { threshold: 0.1 }
-        )
-        const target = document.getElementById("main-sections");
-        if (target) {
-            observer.observe(target);
-        }
-        return () => {
-            if (target) {
-                observer.unobserve(target);
-            }
-        }
-    }, [])
-    return (
-        <section className={`unexpanded-home-grid flex flex-col-reverse md:grid ${expanded ? "expanded-home-grid" : ""}`}>
-            <div>
-                <Front logo={front.logo} message={front.message} label={front.label}  />
-                <div id="main-sections">
-                    <Menu sectionName={menu.sectionName} information={menu.information} sectionButton={menu.sectionButton} />
-                </div>
-            </div>
-            <div className="top-0 right-0 w-full overflow-hidden md:sticky md:h-screen">
-                <img className="reveal-hero-image h-full w-full object-cover" src={images[3].path} alt={images[3].alt} />
-            </div>
-        </section>
-    )
-}
+  // Logic to handle the expanded state of the grid
+  const [expanded, setExpanded] = useState(false);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setExpanded(true);
+          } else {
+            setExpanded(false);
+          }
+        });
+      },
+      { threshold: 0.1 },
+    );
+    const target = document.getElementById("main-sections");
+    if (target) {
+      observer.observe(target);
+    }
+    return () => {
+      if (target) {
+        observer.unobserve(target);
+      }
+    };
+  }, []);
+  return (
+    <section
+      className={`unexpanded-home-grid flex flex-col-reverse md:grid ${expanded ? "expanded-home-grid" : ""}`}
+    >
+      <div>
+        <Front logo={front.logo} message={front.message} label={front.label} />
+        <div id="main-sections">
+          <Menu
+            sectionName={menu.sectionName}
+            information={menu.information}
+            sectionButton={menu.sectionButton}
+          />
+        </div>
+      </div>
+      <div className="top-0 right-0 w-full overflow-hidden md:sticky md:h-screen">
+        <img
+          className="reveal-hero-image h-full w-full object-cover"
+          src={images[3].path}
+          alt={images[3].alt}
+        />
+      </div>
+    </section>
+  );
+};
 
 export default HomeGrid;
--- frontend/src/components/home/Menu.jsx
+++ frontend/src/components/home/Menu.jsx
@@ -4,31 +4,99 @@
  * @author Adrian Cardona Candil
  */
 
+import divider from "../../assets/divider.svg";
+import images from "../../assets/images/export";
+
+/**
+ * @function ToLeftMarquee
+ * @description Renders a marquee of images that scrolls to the left.
+ * @returns {JSX.Element} The marquee component.
+ */
+
+const ToLeftMarquee = () => {
+    return (
+        <div className="relative h-75 overflow-hidden md:h-100">
+            <div className="menu-scroll-left absolute flex h-full w-max gap-7.5">
+                {images.map((image) => (
+                    <img
+                        className="object-cover"
+                        key={image.id}
+                        src={image.path}
+                        alt={image.alt}
+                    ></img>
+                ))}
+                {images.map((image) => (
+                    <img
+                        className="object-cover"
+                        key={image.id}
+                        src={image.path}
+                        alt={image.alt}
+                    ></img>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+/**
+ * @function ToRightMarquee
+ * @description Renders a marquee of images that scrolls to the right.
+ * @returns {JSX.Element} The marquee component.
+ */
+
+const ToRightMarquee = () => {
+    return (
+        <div className="relative h-25 overflow-hidden md:h-50">
+            <div className="menu-scroll-right absolute flex h-full w-max gap-7.5">
+                {images.map((image) => (
+                    <img
+                        className="object-cover"
+                        key={image.id}
+                        src={image.path}
+                        alt={image.alt}
+                    ></img>
+                ))}
+                {images.map((image) => (
+                    <img
+                        className="object-cover"
+                        key={image.id}
+                        src={image.path}
+                        alt={image.alt}
+                    ></img>
+                ))}
+            </div>
+        </div>
+    )
+}
+
 /**
  * @function Menu
  * @description Menu component. Renders the menu section of the home page.
  * @returns {JSX.Element} The menu component.
  */
 
-const Menu = ({sectionName, information, sectionButton}) => {
-    return (
-        <section className="flex flex-col justify-between h-screen min-h-150">
-            <div className="p-15">
-                <button className="inset-ring-2 inset-ring-[var(--primary-light)] dark:inset-ring-[var(--primary-dark)] px-7.5 py-2 text-sm font-bold">{sectionName}</button>
-            </div>
-            <div>
-                <h2>{information.title}</h2>
-                <p>{information.text}</p>
-                <img></img>
-                <div>
-                    <img></img>
-                </div>
-            </div>
-            <div>
-                <button>{sectionButton}</button>
-            </div>
-        </section>
-    )
-}
+const Menu = ({ sectionName, information, sectionButton }) => {
+  return (
+    <section className="flex min-h-screen flex-col justify-between">
+      <div className="p-12.5">
+        <button className="px-7.5 py-2 text-sm font-bold inset-ring-2 inset-ring-[var(--primary-light)] dark:inset-ring-[var(--primary-dark)]">
+          {sectionName}
+        </button>
+      </div>
+      <div className="flex flex-col gap-10 px-10 md:px-15">
+        <h2 style={{ fontFamily: "'Dancing Script'" }} className="text-4xl">
+          {information.title}
+        </h2>
+        <p className="leading-8 font-thin">{information.text}</p>
+        <img src={divider} alt="section divider"></img>
+        <ToLeftMarquee />
+        <ToRightMarquee />
+      </div>
+      <div className="mt-20 grid place-content-center bg-[var(--surface-container-light)] py-12.5 text-[var(--on-primary-container-light)] underline underline-offset-5 transition-all duration-1000 hover:bg-[var(--primary-container-light)] md:py-17.5 dark:bg-[var(--surface-container-dark)] dark:text-[var(--on-primary-container-dark)] hover:dark:bg-[var(--primary-container-dark)]">
+        <button>{sectionButton}</button>
+      </div>
+    </section>
+  );
+};
 
 export default Menu;
