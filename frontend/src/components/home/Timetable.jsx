fdf44c07337df4dd3c73e1946337acf49384e0b1
AdrianCardonaCandil <adriancardonacandil@gmail.com>
2025-05-09 22:58:25 +0100

    menor corrections to timetable

--- frontend/src/components/home/Timetable.jsx
+++ frontend/src/components/home/Timetable.jsx
@@ -14,7 +14,7 @@ import divider from "../../assets/divider.svg";
 
 const Timetable = ({ sectionName, timetable }) => {
     return (
-        <div className="mt-10 flex flex-col gap-15 px-10 md:px-35 2xl:w-2/3">
+        <div className="mt-10 flex flex-col gap-15 px-10 md:px-35 xl:w-2/3">
             <div className="self-end">
                 <button className="px-7.5 py-2 font-bold text-[var(--primary-light)] inset-ring-2 inset-ring-[var(--primary-light)] dark:text-[var(--primary-dark)] dark:inset-ring-[var(--primary-dark)]">
                     {sectionName}
@@ -24,7 +24,7 @@ const Timetable = ({ sectionName, timetable }) => {
                 {timetable.map((entrie) => (
                     <div className="flex flex-col gap-5 md:gap-10">
                         <div className="flex items-center justify-between">
-                            <p style={{ fontFamily: "'Dancing Script'" }}>
+                            <p className="text-2xl" style={{ fontFamily: "'Dancing Script'" }}>
                                 {entrie.days}
                             </p>
                             <p className="font-thin">{entrie.hours}</p>
